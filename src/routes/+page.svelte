4e69bb9db93f0bab8387834d03aeca3a5b7dccd4
PleahMaCaka <pleahmacaka@gmail.com>
2024-03-25 13:29:35 +0900

    feat: add footer and modify navbar for landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1 class="text-2xl italic">Welcome to SvelteKit Supabase Boilerplate!</h1>
-<button class="btn">DaisyUI Button</button>
+<div class="navbar bg-base-100 shadow-xl rounded-b-box px-4">
+    <div class="navbar-start">
+        <a class="btn btn-ghost text-xl" href="/">Landing</a>
+    </div>
+    <div class="navbar-end lg:flex">
+        <ul class="menu menu-horizontal px-1">
+            <li><a href="/about">About</a></li>
+            <li><a href="/signin">Login</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="flex flex-col justify-center pt-24">
+    <h1 class="self-center text-center font-bold text-primary
+               text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+        Build The Enterprise-Grade<br/>Web Application
+    </h1>
+    <p class="self-center text-center text-secondary opacity-65
+              mt-8 text-md sm:text-lg lg:text-2xl">
+        Build the web application with the latest technology stack and best practices.
+    </p>
+    <button class="btn btn-primary w-max px-8 self-center rounded-3xl mt-16">
+        <a href="/app" class="text-white text-2xl font-normal">
+            Getting Started
+        </a>
+    </button>
+</div>
