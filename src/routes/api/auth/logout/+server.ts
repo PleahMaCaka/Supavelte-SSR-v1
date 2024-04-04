dcf040c55ea103a03b7aca6af5d5b7776ec7c377
PleahMaCaka <pleahmacaka@gmail.com>
2024-04-04 14:38:19 +0900

    feat(api/auth): modify the redirection path after logout operation

--- src/routes/api/auth/logout/+server.ts
+++ src/routes/api/auth/logout/+server.ts
@@ -6,5 +6,5 @@ export const GET: RequestHandler = async ({ cookies }) => {
     cookies.delete("access-token", { path: "/" })
     cookies.delete("refresh-token", { path: "/" })
 
-    return redirect(302, "/signin")
+    return redirect(302, "/")
 }
