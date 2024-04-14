18621f652e40a1b6dcfc509292ada3bce4424382
PleahMaCaka <pleahmacaka@gmail.com>
2024-04-15 03:04:11 +0900

    feat: change adapter node to static

--- src/lib/Supabase.ts
+++ src/lib/Supabase.ts
@@ -3,8 +3,8 @@ import { createClient, type User } from "@supabase/supabase-js"
 import type { RequestEvent } from "@sveltejs/kit"
 
 export const supabase = createClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_KEY
+    import.meta.env.VITE_PUBLIC_SUPABASE_URL,
+    import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY,
 )
 
 export async function isAuthorizedUser(event: RequestEvent): Promise<User | null> {
